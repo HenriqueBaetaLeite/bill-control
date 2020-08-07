d72f1ce864fc9696353b9e530c104a0fd5936858
henrique <hbaetaleite@gmail.com>
2020-08-07 13:20:54 -0300

    Updates

---
+++ precache-manifest.706613f58c9ba7df0f313c90d5ca95b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538b13a75fc96e29ab7cd9866dcd2842",
+    "url": "/bill-control/index.html"
+  },
+  {
+    "revision": "bd7e2388b4b490be2082",
+    "url": "/bill-control/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf3cebb89db4855a2cc4",
+    "url": "/bill-control/static/js/2.549a5cb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bill-control/static/js/2.549a5cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7e2388b4b490be2082",
+    "url": "/bill-control/static/js/main.706e6a69.chunk.js"
+  },
+  {
+    "revision": "508c251ab50d60637d3f",
+    "url": "/bill-control/static/js/runtime-main.28a7c74b.js"
+  }
+]);
