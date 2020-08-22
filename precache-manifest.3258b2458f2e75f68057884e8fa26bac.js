5811ee685bb718ca547669e214d14729acc1f58a
henrique <hbaetaleite@gmail.com>
2020-08-22 19:53:50 -0300

    Updates

---
+++ precache-manifest.3258b2458f2e75f68057884e8fa26bac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6cba0ce648b9d930268f11aec43e86",
+    "url": "/bill-control/index.html"
+  },
+  {
+    "revision": "aad957e2ee25d3f3726f",
+    "url": "/bill-control/static/js/2.d819c747.chunk.js"
+  },
+  {
+    "revision": "0fb1ab9166e4090d40b886200af7baec",
+    "url": "/bill-control/static/js/2.d819c747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4aa55380b7a1a7d95ed",
+    "url": "/bill-control/static/js/main.117f7a4c.chunk.js"
+  },
+  {
+    "revision": "508c251ab50d60637d3f",
+    "url": "/bill-control/static/js/runtime-main.28a7c74b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bill-control/precache-manifest.84d82217209a7931f9ed164fd87e0fbe.js"
+  "/bill-control/precache-manifest.3258b2458f2e75f68057884e8fa26bac.js"
 );
 
 self.addEventListener('message', (event) => {
