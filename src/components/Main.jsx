0a7965876f2a5082f15e1c933f8c6468dc041bb1
henrique <hbaetaleite@gmail.com>
2020-08-25 11:50:53 -0300

    adicionando balanco

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -32,18 +32,18 @@ const Main = () => {
   const valorReceitas =
     entries.length === 0
       ? 0
-      : entries
-          .map((entry) => Number(entry.valor))
-          .reduce((acc, tot) => acc + tot)
-          .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+      : entries.map((entry) => Number(entry.valor)).reduce((acc, tot) => acc + tot);
 
   const valorDespesas =
     userOut.length === 0
       ? 0
-      : userOut
-          .map((out) => Number(out.valor))
-          .reduce((acc, tot) => acc + tot)
-          .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+      : userOut.map((out) => Number(out.valor)).reduce((acc, tot) => acc + tot);
+
+  const saldo = valorReceitas - valorDespesas;
+
+  const styleValor = {
+    color: saldo < 0 ? 'red' : 'green',
+  };
 
   return (
     <main className="container">
@@ -62,8 +62,20 @@ const Main = () => {
         Você possui {userOut.length} registro(s) de despesas e {entries.length} registro(s) de
         receitas
       </p>
-      <p>Sua(s) receita(s) tem um total de {valorReceitas}</p>
-      <p>Sua(s) despesa(s) tem um total de {valorDespesas}</p>
+      <p>
+        Sua(s) receita(s) tem um total de{' '}
+        {valorReceitas.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+      </p>
+      <p>
+        Sua(s) despesa(s) tem um total de{' '}
+        {valorDespesas.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+      </p>
+      <p>
+        Seu balanço é de{' '}
+        <span style={styleValor}>
+          {saldo.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+        </span>
+      </p>
 
       <Link className="btn btn-dark mr-3" to="/controle-despesas">
         Minhas despesas
